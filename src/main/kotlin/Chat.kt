4158ce97fb40f4ce659571e2da21ae3d8adf3bab
afloemkis <chere.aleksandra@gmail.com>
2023-07-26 14:39:59 +0300

    functions updated

--- src/main/kotlin/Chat.kt
+++ src/main/kotlin/Chat.kt
@@ -1,6 +1,6 @@
-data class Message(val id: Int, val text: String, val senderId: Int, val chatId: Int)
+data class Message(val id: Int, var text: String, val senderId: Int, val chatId: Int, var unread: Boolean = true)
 
-data class Chat(val id: Int, val userIds: List<Int>, val messages: MutableList<Message> = mutableListOf())
+data class Chat(val id: Int, var userIds: List<Int>, val messages: MutableList<Message> = mutableListOf())
 
 class ChatNotFoundException(message: String) : Exception(message)
 class MessageNotFoundException(message: String) : Exception(message)
@@ -9,10 +9,11 @@ class ChatService {
     private val chats = mutableListOf<Chat>()
 
     // Создание нового чата при отправке первого сообщения
-    fun createChat(senderId: Int, receiverId: Int, text: String): Chat {
-        val newChat = Chat(chats.size + 1, listOf(senderId, receiverId))
+   fun createChat(senderId: Int, receiverId: Int, text: String): Chat {
+        val newChatId = getNextId()
+        val newChat = Chat(newChatId, listOf(senderId, receiverId))
         chats.add(newChat)
-        sendMessage(senderId, newChat.id, text)
+        newChat.messages.add(Message(getNextMid(newChatId), text, senderId, newChatId))
         return newChat
     }
 
@@ -21,31 +22,32 @@ class ChatService {
         return chats.filter { it.userIds.contains(userId) }
     }
 
+    //Получение списка непрочитанных чатов
+    fun getUnreadChats(userId: Int): List<Chat> {
+        return getChats(userId).filter { chat -> chat.messages.any { it.unread } }
+    }
+
     // Получение списка последних сообщений из чатов (можно в виде списка строк)
     fun getLastMessages(userId: Int): List<String> {
-        return chats
-            .filter { it.userIds.contains(userId) && it.messages.isNotEmpty() }
-            .map { chat ->
-                chat.messages.last().text
-            }
+        return chats.map { it.messages.lastOrNull()?.text ?: "нет сообщений" }
     }
 
     // Получение списка сообщений из чата, начиная с заданного messageId
-    fun getMessagesFromChat(chatId: Int, messageId: Int, count: Int): List<Message> {
+    fun getMessagesFromChat(userId: Int, chatId: Int, messageId: Int, count: Int): List<Message> {
         val chat = getChatById(chatId)
-        val startIndex = chat.messages.indexOfFirst { it.id == messageId } + 1
-        val endIndex = startIndex + count.coerceAtMost(chat.messages.size - startIndex)
-        val messages = chat.messages.subList(startIndex, endIndex)
-        messages.forEach { it.senderId } // Отмечаем сообщения как прочитанные
-        return messages
+        return chat.messages.filter { it.id >= messageId }.take(count).onEach  { it.unread = false } // Отмечаем сообщения как прочитанные
     }
 
     // Отправка сообщения
-    fun sendMessage(senderId: Int, chatId: Int, text: String) {
-        val chat = getChatById(chatId)
-        val messageId = chat.messages.size + 1
-        val newMessage = Message(messageId, text, senderId, chatId)
-        chat.messages.add(newMessage)
+    fun sendMessage(senderId: Int, receiverId: Int, chatId: Int? = null, text: String) {
+        if (chatId != null && chats.any { it.id == chatId }) {
+            val chat = getChatById(chatId)
+            val messageId = getNextMid(chatId)
+            val newMessage = Message(messageId, text, senderId, chatId)
+            chat.messages.add(newMessage)
+        } else {
+            createChat(senderId, receiverId, text)
+        }
     }
 
     // Удаление сообщения
@@ -76,4 +78,18 @@ class ChatService {
         return chats.flatMap { it.messages }.find { it.id == messageId }
             ?: throw MessageNotFoundException("Message with ID $messageId not found")
     }
+
+    //Вспомогательная функция для генерации нового id чата
+    private fun getNextId(): Int {
+        return chats.maxByOrNull { it.id }?.id?.plus(1) ?: 1
+    }
+
+    //Вспомогательная функция для генерации нового id сообщения
+    private fun getNextMid(chatId: Int): Int {
+        return chats.find {it.id == chatId}?.messages?.maxByOrNull { it.id }?.id?.plus(1) ?: 1
+    }
+
+
+
+
 }
--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -13,12 +13,15 @@ class ChatServiceTest {
     }
 
     @Test
-    fun testGetChats() {
+    fun testGetUnreadChats() {
         val service = ChatService()
-        service.createChat(senderId = 1, receiverId = 2, text = "Hello!")
-        service.createChat(senderId = 1, receiverId = 3, text = "Hi!")
-        val chats = service.getChats(userId = 1)
-        assertEquals(2, chats.size)
+        val newChat1 = service.createChat(senderId = 1, receiverId = 2, text = "Hello!")
+        val newChat = service.createChat(senderId = 1, receiverId = 3, text = "Hi!")
+        service.getMessagesFromChat(userId = 1, chatId = newChat1.id, messageId = 1, count = 2)
+        service.sendMessage(senderId = 1, receiverId = 3, chatId = newChat.id, text = "HelloHello!")
+        val chats = service.getUnreadChats(userId = 1)
+        println(chats)
+        assertEquals(1, chats.size)
     }
 
     @Test
@@ -36,19 +39,21 @@ class ChatServiceTest {
     fun testGetMessagesFromChat() {
         val service = ChatService()
         val chat = service.createChat(senderId = 1, receiverId = 2, text = "Hello!")
-        service.sendMessage(senderId = 2, chatId = chat.id, text = "Hi!")
-        service.sendMessage(senderId = 1, chatId = chat.id, text = "How are you?")
-        val messages = service.getMessagesFromChat(chatId = chat.id, messageId = 1, count = 2)
+        service.sendMessage(senderId = 2, receiverId = 1, chatId = chat.id, text = "Hi!")
+        service.sendMessage(senderId = 1, receiverId = 2, chatId = chat.id, text = "How are you?")
+        val messages = service.getMessagesFromChat(userId = 1, chatId = chat.id, messageId = 2, count = 2)
         assertEquals(2, messages.size)
         assertEquals("Hi!", messages[0].text)
         assertEquals("How are you?", messages[1].text)
+        assertEquals(false, messages[0].unread)
+        assertEquals(false, messages[1].unread)
     }
 
     @Test
     fun testDeleteMessage() {
         val service = ChatService()
         val chat = service.createChat(senderId = 1, receiverId = 2, text = "Hello!")
-        service.sendMessage(senderId = 2, chatId = chat.id, text = "Hi!")
+        service.sendMessage(senderId = 2, receiverId = 1, chatId = chat.id, text = "Hi!")
         service.deleteMessage(userId = 1, messageId = 1)
         assertEquals(1, chat.messages.size)
     }
@@ -72,4 +77,5 @@ class ChatServiceTest {
         val service = ChatService()
         service.deleteMessage(userId = 1, messageId = 1)
     }
+
 }
